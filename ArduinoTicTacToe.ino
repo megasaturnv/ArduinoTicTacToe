4196ef3edc5876af430037f1ef7d6efd3e6ef8dd
megasaturnv <megasaturnv@users.noreply.github.com>
2017-07-14 11:44:44 +0100

    Added thinking time, which position was played and pause at end of game. See README.md for details

--- ArduinoTicTacToe.ino
+++ ArduinoTicTacToe.ino
@@ -1,3 +1,5 @@
+#define BAUD_RATE 9600
+
 int difficulty = 8;
 
 //Byte array to convert keyboard keypad numbers to TicTacToe.ino's original number layout
@@ -5,27 +7,21 @@ int difficulty = 8;
 //255 allows the program to show an error message to the user later in the code
 const byte keypadConversion[] = {255, 6, 7, 8, 3, 4, 5, 0, 1, 2};
 /*
-Old TicTacToe layout:
- 0 | 1 | 2
----+---+---
- 3 | 4 | 5
----+---+---
- 6 | 7 | 8
-
-New layout (Matches keyboard keypad):
- 7 | 8 | 9
----+---+---
- 4 | 5 | 6
----+---+---
- 1 | 2 | 3
+  Old TicTacToe layout:    New layout (Matches keyboard keypad):
+  0 | 1 | 2                7 | 8 | 9
+  ---+---+---              ---+---+---
+  3 | 4 | 5                4 | 5 | 6
+  ---+---+---              ---+---+---
+  6 | 7 | 8                1 | 2 | 3
 */
 
 const byte buttonPins[] = {12, 11, 10, //List of pins connected to 9 physical buttons, Arranged into a 9x9 grid
                            4,  5,  6,  //e.g. top-right tic-tac-toe button is connected to pin 10
-                           7,  8,  9}; //These pins are pulled high with the Arduino's internal pull-up resistors
+                           7,  8,  9
+                          }; //These pins are pulled high with the Arduino's internal pull-up resistors
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(BAUD_RATE);
   //Set pinMode of all pins in buttonPins[] to INPUT_PULLUP
   //Loop for the amount of values in buttonPins
   //In this case, sizeof(byte) == 1 so it's not really needed.
@@ -33,6 +29,8 @@ void setup() {
   for (byte i = 0; i < (sizeof(buttonPins) / sizeof(byte)); i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
+
+  drawNumberedBoard();
 }
 
 bool digitalReadDebounce(int pin) { //Digital read a pin with software debounce
@@ -119,6 +117,7 @@ int minimax(int board[9], int player, int depth) {
 }
 
 void computerMove(int board[9]) {
+  unsigned long computerMoveStartTime = millis();
   Serial.println("Thinking..."); //Say when the computer is thinking (without it the program looks like it has crashed)
   int move = -1;
   int score = -2;
@@ -136,26 +135,28 @@ void computerMove(int board[9]) {
   }
   //returns a score based on minimax tree at a given node.
   board[move] = 1;
+
+  Serial.print("Arduino was thinking for "); Serial.print(millis() - computerMoveStartTime); Serial.println(" ms");
 }
 
 void playerMove(int board[9]) {
   byte move = 0;
   do {
-    Serial.print("Input move ([1..9]): ");
+    Serial.println("Input move ([1..9]): ");
 
     bool waitingForPlayerMove = true;
     while (waitingForPlayerMove) {
       if (Serial.available() > 0) {
         byte moveKeypadNum = Serial.parseInt();
         if (moveKeypadNum <= 9) { //if keypad number is less than or equal to 9, the move is valid. 'moveKeypadNum' cannot be negative ( < 0 ) because it is a 'byte' type not 'int'.
-                                  //If the user types '0', keypadConversion[0] = 255 which is also an invalid move (So the error message will appear)
+          //If the user types '0', keypadConversion[0] = 255 which is also an invalid move (So the error message will appear)
           move = keypadConversion[moveKeypadNum];
           waitingForPlayerMove = false;
         } else {
           move = 255; //Set 'move' to 255 which will be picked up later as an invalid move
         }
       }
-      for (byte i=0; i <= 8; i++) {
+      for (byte i = 0; i <= 8; i++) {
         if (digitalReadDebounce(buttonPins[i])) {
           move = i;
           waitingForPlayerMove = false;
@@ -169,46 +170,56 @@ void playerMove(int board[9]) {
 
   } while ((move > 8 || move < 0) || board[move] != 0);
   board[move] = -1;
+
+  //Reverse lookup in keypadConversion[] array
+  Serial.print("You played X in position ");
+  for (int i = 1; i <= 9; i++) { //For position [1] to [9] in keypadConversion[] array... (the first position, [0] is not used in keypadConversion[])
+    if (move == keypadConversion[i]) { //If the selected move == the current value selected in the array
+      Serial.println(i); //Print where we are in the array ( i )
+      break; //There's no need to continue the for loop, so break out of it
+    }
+  }
 }
 
 void loop() {
   int board[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
   Serial.println("====================================");
-  drawNumberedBoard();
-  Serial.println("Would you like to play X(type '1' or press left buttons) or O(type '2' or press right buttons)? ");
+  Serial.println("Would you like to play 1st (type '1' or press left buttons) or 2nd(type '2' or press right buttons)?");
 
   byte player;
-  bool waitingToStartGame = true;
-  while (waitingToStartGame) {
+  bool waiting = true;
+  while (waiting) {
     if (Serial.available() > 0) {
       player = Serial.parseInt();
-      waitingToStartGame = false;
+      waiting = false;
     }
     //Loop for the amount of values in buttonPins
     if (digitalReadDebounce(buttonPins[0]) || digitalReadDebounce(buttonPins[3]) || digitalReadDebounce(buttonPins[6])) { //If player presses one of the left buttons
-       player = 1;
-       waitingToStartGame = false;
+      player = 1;
+      waiting = false;
     } else if (digitalReadDebounce(buttonPins[2]) || digitalReadDebounce(buttonPins[5]) || digitalReadDebounce(buttonPins[8])) { //If player presses one of the right buttons
-       player = 2;
-       waitingToStartGame = false;
+      player = 2;
+      waiting = false;
     }
   }
   if (player != 1 && player != 2) { //If user doesn't select a valid player type
     Serial.print("Invalid player number received. Picking at random... "); //Notify the player
     randomSeed(millis()); //Set random seed to millis()
     player = random(1, 3); //Set player type as '1' or '2' randomly. '1' = 'X' and '2' = 'O'.
-                           /*random(1, 3); is used because the max range of random is (3 - 1 = 2)
-                             To quote the Arduino docs: (https://www.arduino.cc/en/Reference/Random)
+    /*random(1, 3); is used because the max range of random is (3 - 1 = 2)
+      To quote the Arduino docs: (https://www.arduino.cc/en/Reference/Random)
 
-                             Parameters
-                                 min - lower bound of the random value, inclusive (optional)
-                                 max - upper bound of the random value, exclusive
+      Parameters
+          min - lower bound of the random value, inclusive (optional)
+          max - upper bound of the random value, exclusive
 
-                             Returns
-                                 a random number between min and max-1 (long) */
+      Returns
+          a random number between min and max-1 (long) */
   }
   //Say what the player is playing as
-  Serial.print("You will play as: "); Serial.println(player == 1 ? "X" : "O"); //If player == 1, print "X". Else, print "O"
+  Serial.print("You will play: ");
+  Serial.print(player == 1 ? "1st" : "2nd"); //If player == 1, print "X". Else, print "O"
+  Serial.println(" as 'X'");
 
   unsigned turn;
   for (turn = 0; turn < 9 && win(board) == 0; ++turn) {
@@ -231,4 +242,22 @@ void loop() {
       Serial.println("You win!");
       break;
   }
+
+  Serial.println("Type anything or press any button to continue\n");
+  waiting = true;
+  while (waiting) {
+    if (Serial.available() > 0) { //If user typed anything
+      waiting = false;
+      delay(10);
+      while (Serial.available() > 0) { //Clear out the serial buffer
+        char t = Serial.read();
+      }
+    }
+    for (byte i = 0; i <= 8; i++) {
+      if (digitalReadDebounce(buttonPins[i])) { //If user pressed a button
+        waiting = false;
+      }
+    }
+  }
+  delay(500); //Extra delay to make sure user has not debounced the button a second time (after digitalReadDebounce())
 }
